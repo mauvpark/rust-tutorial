17df20f7bfae991a02ed3b5e7d12ffc4e6c71dd4
Mauv-hub <mauvpark@gmail.com>
2021-09-20 19:40:23 +0900

    first commit

---
+++ Chapter2/guessing_game/src/main.rs
@@ -0,0 +1,39 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io; // ? import {io} from 'std'
+
+fn main() {
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    println!("The secret number is: {}", secret_number);
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line"); // ? import {stdin()} from 'io';
+
+        // ? This guess has same name with above guess but has different object.
+        // ? It has benefit because we don't have to consider other variable name.
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            },
+        }
+    }
+}
